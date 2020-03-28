73d3c27dd2ac6b83e53d059f7ba78856cb9254ba
arte921 <ardjanve@gmail.com>
2020-03-28 20:29:02 +0100

    autocommit

--- init.lua
+++ init.lua
@@ -2,7 +2,8 @@ local http = minetest.request_http_api()
 
 dofile(minetest.get_modpath("realmap") .. "/keys.lua")
 
-local mapsize = 40000
+local mapsize = 20000000
+
 
 if sflat == nil then sflat = {} end
 sflat.options = {
@@ -15,58 +16,61 @@ minetest.register_on_mapgen_init(function(mgparams)
 end)
 
 minetest.register_on_generated(function(minp, maxp, seed)
-
-	if minp.x < -mapsize * 2 or maxp.x > mapsize * 2 or minp.z < -mapsize or maxp.z > mapsize then
-		return
-	end
-
-	local t1 = os.clock()
-	local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
-	local area = VoxelArea:new{MinEdge = emin, MaxEdge = emax}
-	local data = vm:get_data()
-	
-	argument = ""
-
 	function doTile(minx,maxx,minz,maxz)
-		
+
+		local lxarg = minx/mapsize/2 * 180
+		local uxarg = maxx/mapsize/2 * 180
 		local lzarg = minz/mapsize * 90
 		local uzarg = maxz/mapsize * 90
-		local lxarg = minx/mapsize * 180
-		local uxarg = maxx/mapsize * 180
-		if math.abs(lzarg) >= 85 then lzarg = 0 end
-		if math.abs(uzarg) >= 85 then uzarg = 0 end
+
+		if math.abs(lxarg) > 180 then return end
+		if math.abs(uxarg) > 180 then return end
+
+		if math.abs(lzarg) > 85 then return end
+		if math.abs(uzarg) > 85 then return end
 
 
-		local murl = "http://dev.virtualearth.net/REST/v1/Elevation/Bounds?bounds=" .. lxarg .. "," .. lzarg .. "," .. uxarg .. "," .. uzarg .. "&rows=" .. math.abs(maxz-minz)+1 .. "&cols=" .. math.abs(maxx-minx)+1 .. "&key=" .. bingApiKey 
+
+		local murl = "http://dev.virtualearth.net/REST/v1/Elevation/Bounds?bounds=" .. lzarg .. "," .. lxarg .. "," .. uzarg .. "," .. uxarg .. "&rows=" .. math.abs(maxz-minz)+1 .. "&cols=" .. math.abs(maxx-minx)+1 .. "&key=" .. bingApiKey 
 		--minetest.log(murl)
-
 		http.fetch({url = murl,timeout = 10},function(response)
-			if response["completed"] == true then 
-				minetest.log(response["data"])
+
+			function doit() 
+				--minetest.log(response["data"])
 
 				local jo = minetest.parse_json(response["data"])
 				local ja = jo["resourceSets"][1]["resources"][1]["elevations"]
 
 				local k = 0
-				for z = maxz,minz,-1 do
+				for z = minz,maxz do
 					for x = minx,maxx do
 						k = k + 1
-						if ja[k] >= minp.y and ja[k] <= maxp.y then
+						
+						--if ja[k] >= minp.y and ja[k] <= maxp.y then
 							local y = ja[k]
-							local vi = area:index(x, y, z)
-							--data[vi] = minetest.get_content_id("default:dirt_with_grass")
-							--minetest.log("k="..k.." z="..z.." x="..x)
-						end
+
+							--minetest.log("x"..x.."z"..z.."y"..y)
+							
+							--minetest.log(y)
+							minetest.set_node({x=x,y=y,z=z},{name="default:dirt_with_grass"})
+						--end
 
 					end
 				end
-
-				vm:set_data(data)
-				vm:set_lighting({day = 0, night = 0})
-				vm:update_liquids() 
-				vm:calc_lighting()
-				vm:write_to_map(data)
 			end
+
+			function tryit()
+				if not pcall(doit) then 
+					minetest.after(math.random()*2,tryit)
+					minetest.log("api is rate-limiting")
+				 end
+			end
+
+			if minx > -mapsize * 2 and maxx < mapsize * 2 and minz > -mapsize and maxz < mapsize then
+				tryit()
+			end
+			
+
 		end)
 	end
 
@@ -74,33 +78,66 @@ minetest.register_on_generated(function(minp, maxp, seed)
 	local totalwidth = math.abs(maxp.x - minp.x)
 	local xtiles = math.floor(totalwidth/tile)
 
-	minetest.log("totalwidth " .. totalwidth .. "xtiles " .. xtiles)
+	local mxi = 0
+	local mzi = 0
 
-	doTile(
-		minp.x,
-		minp.x + tile,
-		minp.z,
-		minp.z + tile
-	)
+	local tileSize = 30
+
+	local xtiles = math.floor(math.abs(maxp.x-minp.x)/tileSize)
+	local ztiles = math.floor(math.abs(maxp.z-minp.z)/tileSize)
+
+	if math.sqrt(2) * mapsize > tileSize then
+
+		for xi = 0,xtiles-1 do
+			for zi = 0,ztiles-1 do
+				doTile(
+					minp.x + xi * tileSize,
+					minp.x + (xi + 1) * tileSize,
+					minp.z + zi * tileSize,
+					minp.z + (zi + 1) * tileSize
+				)
+			end
+		end
+
+		for xi = 0,xtiles-1 do
+			for zi = 0,ztiles-1 do
+				doTile(
+					minp.x + xi * tileSize,
+					minp.x + (xi + 1) * tileSize,
+					minp.z + ztiles * tileSize,
+					maxp.z
+				)
+			end
+		end
+
+		for zi = 0,ztiles-1 do
+			doTile(
+				minp.x + xtiles * tileSize,
+				maxp.x,
+				minp.z + zi * tileSize,
+				minp.z + (zi + 1) * tileSize
+			)
+		end
+
+		doTile(
+			minp.x + xtiles * tileSize,
+			maxp.x,
+			minp.z + ztiles * tileSize,
+			maxp.z
+		)
+
+	else
+		doTile(
+			minp.x,
+			maxp.x,
+			minp.z,
+			maxp.z
+		)
+
+	end
 
 
 
-
-
-
-
-	--minp.x < -mapsize * 2 or maxp.x > mapsize * 2 or minp.z < -mapsize or maxp.z
-	
-	
-		
-	
-
-
-
-
-
-
-	
 
 end)
 
